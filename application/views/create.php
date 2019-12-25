bb34391cca577d4c8104a5dd036db952c67c2cd4
caturbgs <caturbgs@github.com>
2019-12-25 22:22:53 +0700

    view create

--- application/views/create.php
+++ application/views/create.php
@@ -3,10 +3,58 @@
 		<h1>Menu Utama</h1>
 		<div class="col-md-10">
 			<div class="row justify-content-center">
-				<div class="col-md-3">
-					<a href="Supplier/history" class="btn-home btn btn-block btn-success"><i class="fa fa-history"></i> Riwayat</a>
-					<a href="Supplier/history" class="btn-home btn btn-block btn-success"><i class="fa fa-folder"></i> Buka File</a>
-					<a href="Supplier/create" class="btn-home btn btn-block btn-success"><i class="fa fa-pencil"></i> Buat File</a>
+				<div class="table-responsive">
+					<table class="table table-bordered table-hovered">
+						<thead>
+							<th>NAMA SUPPLIER</th>
+							<th>DISKON</th>
+							<th>PELAYANAN</th>
+							<th>KUALITAS KAROSESI</th>
+							<th>PROGRAM SERVIS BERKALA</th>
+							<th>BONUS</th>
+						</thead>
+						<tbody>
+							<?php 
+								// Data Supplier
+								foreach($supplier as $s){
+									$id = $s['id'];
+									$nama = $s['nama'];
+									$diskon = $s['diskon'];
+									$pelayanan = $s['pelayanan'];
+									$karoseri = $s['karoseri'];
+									$program_servis = $s['program_servis'];
+									$bonus = $s['bonus'];
+
+									echo "<tr>";
+									echo "<td>
+									<span class='edit' >".$nama."</span>
+									<input type='text' class='txtedit' data-id='".$id."' data-field='name' id='nametxt_".$id."' value='".$nama."' >
+									</td>";
+									echo "<td>
+									<span class='edit' >".$diskon."</span>
+									<input type='text' class='txtedit' data-id='".$id."' data-field='diskon' id='diskontxt_".$id."' value='".$diskon."' >
+									</td>";
+									echo "<td>
+									<span class='edit' >".$pelayanan."</span>
+									<input type='text' class='txtedit' data-id='".$id."' data-field='pelayanan' id='pelayanantxt_".$id."' value='".$pelayanan."' >
+									</td>";
+									echo "<td>
+									<span class='edit' >".$karoseri."</span>
+									<input type='text' class='txtedit' data-id='".$id."' data-field='karoseri' id='karoseritxt_".$id."' value='".$karoseri."' >
+									</td>";
+									echo "<td>
+									<span class='edit' >".$program_servis."</span>
+									<input type='text' class='txtedit' data-id='".$id."' data-field='program_servis' id='program_servistxt_".$id."' value='".$program_servis."' >
+									</td>";
+									echo "<td>
+									<span class='edit' >".$bonus."</span>
+									<input type='text' class='txtedit' data-id='".$id."' data-field='bonus' id='bonustxt_".$id."' value='".$bonus."' >
+									</td>";
+									echo "</tr>";
+								}
+								?>
+						</tbody>
+					</table>
 				</div>
 			</div>
 		</div>
